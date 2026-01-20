5ba937cffe784a877f774bbab5f9ecef4241a114
Anil1453 <124776397+Anil1453@users.noreply.github.com>
2026-01-20 13:25:34 +0200

    Add files via upload

---
+++ Models/BookViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookFavorites.Models
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+        public List<SelectListItem> Categories { get; set; }
+        public IFormFile Image {  get; set; }
+        public string ImageURL { get; set; }
+
+    }
+}
---
+++ Models/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookFavorites.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
